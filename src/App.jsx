8f56f876f68e2df2911b2aa751f35baacdc93117
RosarioManny <rosario.emm47@gmail.com>
2024-10-15 18:01:15 -0400

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,67 @@
-import { weatherForecasts } from "./WeatherForecaset/WeatherForecast.jsx";  
-import './App.css';
+import "./App.css";
+import WeatherForecast from "./components/WeatherForecast";
 
 const App = () => {
-  
+  const weatherForecasts = [
+    {
+      day: "Mon",
+      img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
+      imgAlt: "sun icon",
+      conditions: "sunny",
+      time: "Morning"
+    },
+    {
+      day: "Tue",
+      img:
+        "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+      imgAlt: "moon icon",
+      conditions: "clear",
+      time: "Night"
+    },
+    {
+      day: "Wed",
+      img:
+        "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+      imgAlt: "clouds with lightning icon",
+      conditions: "stormy",
+      time: "All Day"
+    },
+    {
+      day: "Thu",
+      img:
+        "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
+      imgAlt: "sun overcast by clouds icon",
+      conditions: "overcast",
+      time: "Evening"
+    },
+    {
+      day: "Fri",
+      img:
+        "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
+      imgAlt: "moon overcast by clouds icon",
+      conditions: "cloudy",
+      time: "Night"
+    }
+  ];
+
   return (
     <>
-    <h1>Local Weather</h1>
-    <section>
-      <div className="weather">
-        <h2>Day of the Week</h2>
-        <img src="" alt="" />
-        <p><span>conditions: </span>current weather conditions</p>
-        <p><span>time: </span>time of day</p>
-      </div>
-    </section>
-    </>
-  );
-}
+  <h1>Local Weather</h1>
+  <section>
+    {weatherForecasts.map((forecast, idx) => (
+          <WeatherForecast 
+            key={idx}
+            day={forecast.day}
+            img={forecast.img}
+            imgAlt={forecast.imgAlt}
+            conditions={forecast.conditions}
+            time={forecast.time}
+          />
+        ))}
+  </section>
+</>
 
-export default App
+  );
+};
+
+export default App;
